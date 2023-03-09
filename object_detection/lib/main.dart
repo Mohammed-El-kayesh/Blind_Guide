56583728d831acabb139c916098885460cfd925e
Mohammed-El-kayesh <70601162+Mohammed-El-kayesh@users.noreply.github.com>
2023-03-09 21:44:59 +0200

    1

--- object_detection/lib/main.dart
+++ object_detection/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
-import 'package:object_detection/object Detection.dart';
 import 'package:object_detection/pageView.dart';
 
 List<CameraDescription>? cameras;
@@ -20,7 +19,7 @@ class MyApp extends StatelessWidget {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Object Detection',
-      theme: ThemeData(primarySwatch: Colors.blue),
+      theme: ThemeData(primarySwatch: Colors.teal),
       home:  PageViewScreen(),
     );
   }
