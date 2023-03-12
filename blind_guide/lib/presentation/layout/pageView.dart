32cad6902688f45604db4e5226b46aec97681ca8
Amr <amralmohamdy@gmail.com>
2023-03-12 13:49:14 +0200

    Solve Error Text Recognition and add assets/images to pubspec.yaml

--- blind_guide/lib/presentation/layout/pageView.dart
+++ blind_guide/lib/presentation/layout/pageView.dart
@@ -1,19 +1,14 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:blind_guide/bloc/appCubit.dart';
 import 'package:blind_guide/bloc/appState.dart';
 
-
 class PageViewScreen extends StatefulWidget {
-
   @override
   State<PageViewScreen> createState() => _PageViewScreenState();
 }
 
 class _PageViewScreenState extends State<PageViewScreen> {
-
-
   // @override
   // void dispose() {
   //   _pageController!.dispose();
@@ -23,15 +18,16 @@ class _PageViewScreenState extends State<PageViewScreen> {
   @override
   Widget build(BuildContext context) {
     var cubit = BlocProvider.of<AppCubit>(context);
-    return BlocBuilder<AppCubit,AppState>(builder: (context,state)=>Scaffold(
-      appBar: AppBar(
-        title: Text(cubit.titles[cubit.currentPageIndex]),
-      ),
-      body: PageView(
-        controller: cubit.pageController,
-        onPageChanged: (int index)=>cubit.changePageViewIndex(index),
-        children: cubit.screensList,)
-    ));
+    return BlocBuilder<AppCubit, AppState>(
+        builder: (context, state) => Scaffold(
+            appBar: AppBar(
+              title: Text(cubit.titles[cubit.currentPageIndex]),
+            ),
+            body: PageView(
+              controller: cubit.pageController,
+              onPageChanged: (int index) => cubit.changePageViewIndex(index),
+              children: cubit.screensList,
+              pageSnapping: true,
+            )));
   }
-
-  }
+}
