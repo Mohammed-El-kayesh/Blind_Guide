eefd318ded3da37de018f0a26fb87774a13b1cbb
Mohamed Hassan Elkayesh <70601162+Mohammed-El-kayesh@users.noreply.github.com>
2023-03-13 21:52:25 +0200

    Delete textRecognitionState.dart

--- blind_guide/lib/bloc/objectDetectionCubit/textRecognitionState.dart
+++
@@ -1,7 +0,0 @@
-
-abstract class TextRecognitionState{}
-class InitialState extends TextRecognitionState{}
-class InitializeCameraState extends TextRecognitionState{}
-class ChangeIsCamInitializeState extends TextRecognitionState{}
-class ScanTextState extends TextRecognitionState{}
-
