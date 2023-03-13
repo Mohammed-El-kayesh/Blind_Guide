53d3d8ce6397ce9c9441ed6e5fde27eba15abb1a
Mohamed Hassan Elkayesh <70601162+Mohammed-El-kayesh@users.noreply.github.com>
2023-03-13 21:49:58 +0200

    Add files via upload

---
+++ blind_guide/lib/bloc/objectDetectionCubit/textRecognitionState.dart
@@ -0,0 +1,7 @@
+
+abstract class TextRecognitionState{}
+class InitialState extends TextRecognitionState{}
+class InitializeCameraState extends TextRecognitionState{}
+class ChangeIsCamInitializeState extends TextRecognitionState{}
+class ScanTextState extends TextRecognitionState{}
+
